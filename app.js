e43cddc81a3b574c25e7f7983c4f2023e40a5225
nitinjha3 <nitinkumarvk100@gmail.com>
2024-11-10 22:19:17 +0530

    module import export variable and function

---
+++ app.js
@@ -0,0 +1,16 @@
+require("./xyz.js")
+
+var calculateSum=require("./calculateSum.js")
+var name="Namaste NodeJs"
+
+var a=10
+var b=24
+calculateSum(a,b)
+
+ console.log(name);
+//  console.log(a+b);
+ 
+//  console.log(global);
+// console.log(globalThis);
+
+ 
---
+++ calculateSum.js
@@ -0,0 +1,7 @@
+
+// modules protects their variable and function from leaking
+function calculateSum(a,b){
+    console.log(a+b);
+    
+}
+module.exports = calculateSum
